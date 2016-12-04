b71b8b63c7c2deb22c8537b3c13f8f36b6b0a4e9
rockinroyle <rockinroyle@gmail.com>
2016-12-04 09:44:27 -0500

    setup a base stock script for building

--- anykernel.sh
+++ anykernel.sh
@@ -1,21 +1,22 @@
 # AnyKernel2 Ramdisk Mod Script
 # osm0sis @ xda-developers
+#aospdK and tweaks by rockinroyle @ xda-developers
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=
+kernel.string=Shamu-Stock
 do.devicecheck=1
 do.initd=0
 do.modules=0
 do.cleanup=1
-device.name1=
-device.name2=
+device.name1=shamu
+device.name2=Shamu
 device.name3=
 device.name4=
 device.name5=
 
 # shell variables
-block=;
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
 is_slot_device=0;
 
 
