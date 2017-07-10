c46706ca2d8305e4b4c8595fa482e1d604c2bf40
rockinroyle <rockinroyle@gmail.com>
2017-07-10 07:30:43 -0400

    Get a base script working

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=
-do.devicecheck=
-do.modules=
-do.cleanup=
-do.cleanuponabort=
-device.name1=
-device.name2=
+kernel.string=angler-stock
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=angler
+device.name2=Angler
 device.name3=
 device.name4=
 device.name5=
 } # end properties
 
 # shell variables
-block=
+block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
+is_slot_device=0;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
